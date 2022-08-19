48ec60f4cf8e32c61bd3dcc0b2844a0fefe4fdb4
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-19 23:17:56 +0300

    basic layout

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
-console.log('Hello world')
+let myLibrary = [];
+
+function Book(title, author, pages, read) {
+   this.title = title;
+   this.author = author;
+   this.pages = pages;
+   this.read = read;
+   this.info = function () {
+      return `${title} by ${author}, ${pages} pages, ${read ? "read" : "not read yet"}`
+   }
+}
+
+
+function addBookToLibrary() {
+   // do stuff here
+}
