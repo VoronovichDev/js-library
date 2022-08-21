453262a049f054746f69bf441047fb439b8eca0f
Voronovich_dev <alexanderbizv1@gmail.com>
2022-08-21 17:24:32 +0300

    implemented book-adding via form

--- script.js
+++ script.js
@@ -1,5 +1,7 @@
 let myLibrary = [];
 const bookshelf = document.querySelector('.bookshelf')
+let addBookForm = document.forms.addBook
+
 
 function Book(title, author, pages, read) {
    this.title = title;
@@ -12,13 +14,31 @@ function Book(title, author, pages, read) {
 }
 
 
-function addBookToLibrary() {
+addBookForm.addEventListener('submit', e => {
+   e.preventDefault()
+   let title = (document.querySelector('#title__book').value)
+   let author = (document.querySelector('#author__book').value)
+   let pages = (document.querySelector('#pages__book').value)
+   let read = (document.querySelector('#book__read').checked)
+   addBookToLibrary(title, author, pages, read)
+   // console.log(title, author, pages, read)
+   e.target.reset()
+
+   // title = '';
+   // author = '';
+   // pages = '';
+   // read = false;
+
+})
+
+
+function addBookToLibrary(title, author, pages, read) {
    bookshelf.insertAdjacentHTML("afterbegin", `<div class="book">
-   <div class="book__title">Book titlasade</div>
-   <div class="book__author">Book author</div>
-   <div class="book__pages">Book pages</div>
+   <div class="book__title">${title}</div>
+   <div class="book__author">${author}</div>
+   <div class="book__pages">${pages}</div>
    <div class="book__isread">
-      <input type="checkbox">
+      <input type="checkbox" ${read ? "checked" : ""}>
       <label for="" class="read__yes">Yes</label>
       <label for="" class="read__no">No</label>
    </div>
